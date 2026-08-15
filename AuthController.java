64314d5527ce0aaa13d135e011ebaeba02d4910d
THOUFEEKA FATHINA A <thoufeeka96@gmail.com>
2026-08-15 19:29:53 +0530

    Add files via upload

---
+++ AuthController.java
@@ -0,0 +1,42 @@
+package com.college.eventapp.controller;
+
+import com.college.eventapp.model.User;
+import com.college.eventapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Login API (Works for both Admin & Student)
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User loginData) {
+        Optional<User> userOpt = userRepository.findByEmail(loginData.getEmail());
+
+        if (userOpt.isPresent()) {
+            User user = userOpt.get();
+            if (user.getPassword().equals(loginData.getPassword())) {
+                return ResponseEntity.ok(user); // Returns User object with Role (ADMIN/STUDENT)
+            }
+        }
+        return ResponseEntity.badRequest().body("Invalid Email or Password!");
+    }
+
+    // Register User (Admins / Students)
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest().body("Email already registered!");
+        }
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(savedUser);
+    }
+}
